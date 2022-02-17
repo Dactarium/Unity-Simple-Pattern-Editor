6a5f3524c38ee0f8eb3698ec627cd4936e158ebe
Eren Erdoğan <dactarium@outlook.com>
2022-02-17 12:11:19 +0300

    Improved pattern edit
    You can change all horizontal and vertical values same time

--- PatternEditor.cs
+++ PatternEditor.cs
@@ -23,21 +23,32 @@ public class PatternEditor : EditorWindow
     static int[,] intPattern;
     static float[,] floatPattern;
     static string[,] stringPattern;
+    
+    static bool[] boolPatternVertical;
+    static bool[] boolPatternHorizontal;
+    static int[] intPatternVertical;
+    static int[] intPatternHorizontal;
+    static float[] floatPatternVertical;
+    static float[] floatPatternHorizontal;
+    static string[] stringPatternVertical;
+    static string[] stringPatternHorizontal;
+
+    static bool boolPatternAll;
+    static int intPatternAll;
+    static float floatPatternAll;
+    static string stringPatternAll;
+
 
     static TextAsset json;
-    static bool[,] editboolPattern;
-    static int[,] editintPattern;
-    static float[,] editfloatPattern;
-    static string[,] editstringPattern;
     
-    //editor
+    // Editor Settings
     static AreaSelection areaSelection = AreaSelection.New;
     static int fieldWidth = 20;
     static int fieldHeight = 20;
     static bool focusAfterSave = false;
     Vector2 patternScrollPos;
 
-    [MenuItem("Window/Pattern Editor")]
+    [MenuItem("Window/Simple Pattern Editor")]
     public static void ShowWindow()
     {
         GetWindow<PatternEditor>("Pattern Editor");
@@ -48,7 +59,7 @@ public class PatternEditor : EditorWindow
         int width = (int)(Screen.width * 0.95f) - padding;
         int height = (int)(Screen.height * 0.95f) - padding;
 
-        int leftWidth = 220;
+        int leftWidth = 221;
         int leftPosY = padding;
         int selectionHeight = 25;
         int saveHeight = 100;
@@ -113,6 +124,14 @@ public class PatternEditor : EditorWindow
             intPattern = null;
             floatPattern = null;
             stringPattern = null;
+            boolPatternVertical = null;
+            intPatternVertical = null;
+            floatPatternVertical = null;
+            stringPatternVertical = null;
+            boolPatternHorizontal = null;
+            intPatternHorizontal = null;
+            floatPatternHorizontal = null;
+            stringPatternHorizontal = null;
             lenX = 2;
             lenY = 2;
             areaSelection = AreaSelection.New;
@@ -123,6 +142,14 @@ public class PatternEditor : EditorWindow
             intPattern = null;
             floatPattern = null;
             stringPattern = null;
+            boolPatternVertical = null;
+            intPatternVertical = null;
+            floatPatternVertical = null;
+            stringPatternVertical = null;
+            boolPatternHorizontal = null;
+            intPatternHorizontal = null;
+            floatPatternHorizontal = null;
+            stringPatternHorizontal = null;
             lenX = 0;
             lenY = 0;
             areaSelection = AreaSelection.Edit;
@@ -133,46 +160,55 @@ public class PatternEditor : EditorWindow
         GUILayout.Label("Editor input:", GUILayout.ExpandWidth(false));
 
         GUILayout.BeginHorizontal();
-        GUILayout.Label(" Height\t\t", GUILayout.ExpandWidth(false));
-        fieldHeight = EditorGUILayout.IntField(fieldHeight, GUILayout.Width(50));
+        GUILayout.Label(" Width\t\t", GUILayout.ExpandWidth(false));
+        fieldWidth = EditorGUILayout.IntSlider(fieldWidth, 15, 100, GUILayout.Width(105));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
-        GUILayout.Label(" Width\t\t", GUILayout.ExpandWidth(false));
-        fieldWidth = EditorGUILayout.IntField(fieldWidth, GUILayout.Width(50));
+        GUILayout.Label(" Height\t\t", GUILayout.ExpandWidth(false));
+        fieldHeight = EditorGUILayout.IntSlider(fieldHeight, 15, 100, GUILayout.Width(105));
         GUILayout.EndHorizontal();
+       
     }
     void GUINewPatternSetting(){
         GUIEditorSetting();
         
         GUILayout.BeginHorizontal();
         GUILayout.Label("Pattern name\t", GUILayout.ExpandWidth(false));
-        fileName = EditorGUILayout.TextField(fileName, GUILayout.Width(103));
+        fileName = EditorGUILayout.TextField(fileName, GUILayout.Width(105));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
         GUILayout.Label("Pattern type\t", GUILayout.ExpandWidth(false));
-        patternType = (PatternType)EditorGUILayout.EnumPopup(patternType, GUILayout.Width(103));
+        patternType = (PatternType)EditorGUILayout.EnumPopup(patternType, GUILayout.Width(105));
         GUILayout.EndHorizontal();
 
         GUILayout.BeginHorizontal();
         GUILayout.Label("Pattern size\t", GUILayout.ExpandWidth(false));
-        lenX = EditorGUILayout.IntField(lenX, GUILayout.Width(50));
-        lenY = EditorGUILayout.IntField(lenY, GUILayout.Width(50));
+        lenX = EditorGUILayout.IntField(lenX, GUILayout.Width(51));
+        lenY = EditorGUILayout.IntField(lenY, GUILayout.Width(51));
         GUILayout.EndHorizontal();
         
         switch(patternType){
             case PatternType.Bool:
-                boolPattern = setPattern(boolPattern);
+                boolPattern = setPatternArray(boolPattern);
+                boolPatternVertical = setPatternSideArray(boolPatternVertical, true);
+                boolPatternHorizontal = setPatternSideArray(boolPatternHorizontal, false);
                 break;
             case PatternType.Int:
-                intPattern = setPattern(intPattern);
+                intPattern = setPatternArray(intPattern);
+                intPatternVertical = setPatternSideArray(intPatternVertical, true);
+                intPatternHorizontal = setPatternSideArray(intPatternHorizontal, false);
                 break;
             case PatternType.Float:
-                floatPattern = setPattern(floatPattern);
+                floatPattern = setPatternArray(floatPattern);
+                floatPatternVertical = setPatternSideArray(floatPatternVertical, true);
+                floatPatternHorizontal = setPatternSideArray(floatPatternHorizontal, false);
                 break;
             case PatternType.String:
-                stringPattern = setPattern(stringPattern);
+                stringPattern = setPatternArray(stringPattern);
+                stringPatternVertical = setPatternSideArray(stringPatternVertical, true);
+                stringPatternHorizontal = setPatternSideArray(stringPatternHorizontal, false);
                 break;
         }
 
@@ -183,7 +219,7 @@ public class PatternEditor : EditorWindow
 
         GUILayout.BeginHorizontal();
         GUILayout.Label("Pattern File\t", GUILayout.ExpandWidth(false));
-        json = (TextAsset)EditorGUILayout.ObjectField(json, typeof(TextAsset), false, GUILayout.Width(103));
+        json = (TextAsset)EditorGUILayout.ObjectField(json, typeof(TextAsset), false, GUILayout.Width(105));
         GUILayout.EndHorizontal();
         (string, bool) pathData = setPath(path, json);
         
@@ -210,31 +246,59 @@ public class PatternEditor : EditorWindow
                     lenX = _boolPattern.GetLength(0);
                     lenY = _boolPattern.GetLength(1);
                     boolPattern = _boolPattern;
+
+                    boolPatternVertical = setPatternSideArray(boolPatternVertical, true);
+                    boolPatternHorizontal = setPatternSideArray(boolPatternHorizontal, false);
                     break;
                 case PatternType.Int:
                     int[,] _intPattern = dataToPattern<int>(json.text);
                     lenX = _intPattern.GetLength(0);
                     lenY = _intPattern.GetLength(1);
                     intPattern = _intPattern;
+
+                    intPatternVertical = setPatternSideArray(intPatternVertical, true);
+                    intPatternHorizontal = setPatternSideArray(intPatternHorizontal, false);
                     break;
                 case PatternType.Float:
                     float[,] _floatPattern = dataToPattern<float>(json.text);
                     lenX = _floatPattern.GetLength(0);
                     lenY = _floatPattern.GetLength(1);
                     floatPattern = _floatPattern;
+
+                    floatPatternVertical = setPatternSideArray(floatPatternVertical, true);
+                    floatPatternHorizontal = setPatternSideArray(floatPatternHorizontal, false);
                     break;
                 case PatternType.String:
                     string[,] _stringPattern = dataToPattern<string>(json.text);
                     lenX = _stringPattern.GetLength(0);
                     lenY = _stringPattern.GetLength(1);
                     stringPattern = _stringPattern;
+
+                    stringPatternVertical = setPatternSideArray(stringPatternVertical, true);
+                    stringPatternHorizontal = setPatternSideArray(stringPatternHorizontal, false);
                     break;
             }
         }
     }
     void GUIPatternEdit(){
+        GUILayout.BeginHorizontal();
+
+        editAll();
+
+        GUILayout.Space(fieldWidth);
+
+        for(int x = 0; x < lenX; x++){
+            editVeritcal(x);
+        }
+        GUILayout.EndHorizontal();
+
+        GUILayout.Space(fieldHeight);
+
         for(int y = 0; y < lenY; y++){
             GUILayout.BeginHorizontal();
+
+            editHorizontal(y);
+
             for(int x = 0; x < lenX; x++){
 
                 switch(patternType){
@@ -298,6 +362,7 @@ public class PatternEditor : EditorWindow
             if(focusAfterSave){
                 EditorUtility.FocusProjectWindow();
                 Selection.activeObject = AssetDatabase.LoadAssetAtPath<TextAsset>(path);
+                EditorGUIUtility.PingObject(Selection.activeObject);
             }
         }
 
@@ -316,16 +381,194 @@ public class PatternEditor : EditorWindow
         return (path_ != null)?(assetPath, !path_.Equals(assetPath)):(assetPath, true);
     }
 
-    Type[,] setPattern<Type>(Type[,] self, Type[,] pattern){
-        if(self == null)self = pattern;
-        if(self.Length != lenX * lenY) self = pattern;
-        return self;
+    void editAll(){
+        switch(patternType){
+            case PatternType.Bool:
+                bool newBoolValue = EditorGUILayout.Toggle(boolPatternAll, GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newBoolValue != boolPatternAll){
+                    boolPatternAll = newBoolValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        boolPatternHorizontal[y] = newBoolValue;
+                        for(int x = 0; x < lenX; x++){
+                            boolPattern[x,y] = newBoolValue;
+                        }
+                    }
+
+                    for(int x = 0; x < lenX; x++){
+                        boolPatternVertical[x] = newBoolValue;
+                    }
+                }
+                break;
+            case PatternType.Int:
+                int newIntValue = EditorGUILayout.IntField(intPatternAll, GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newIntValue != intPatternAll){
+                    intPatternAll = newIntValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        intPatternHorizontal[y] = newIntValue;
+                        for(int x = 0; x < lenX; x++){
+                            intPattern[x,y] = newIntValue;
+                        }
+                    }
+
+                    for(int x = 0; x < lenX; x++){
+                        intPatternVertical[x] = newIntValue;
+                    }
+                }
+                break;
+            case PatternType.Float:
+                float newFloatValue = EditorGUILayout.FloatField(floatPatternAll, GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newFloatValue != floatPatternAll){
+                    floatPatternAll = newFloatValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        floatPatternHorizontal[y] = newFloatValue;
+                        for(int x = 0; x < lenX; x++){
+                            floatPattern[x,y] = newFloatValue;
+                        }
+                    }
+
+                    for(int x = 0; x < lenX; x++){
+                        floatPatternVertical[x] = newFloatValue;
+                    }
+                }
+                break;
+            case PatternType.String:
+                string newStringValue = EditorGUILayout.TextField(stringPatternAll, GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newStringValue != stringPatternAll){
+                    stringPatternAll = newStringValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        stringPatternHorizontal[y] = newStringValue;
+                        for(int x = 0; x < lenX; x++){
+                            stringPattern[x,y] = newStringValue;
+                        }
+                    }
+
+                    for(int x = 0; x < lenX; x++){
+                        stringPatternVertical[x] = newStringValue;
+                    }
+                }
+                break;
+        }
     }
-    Type[,] setPattern<Type>(Type[,] pattern){
+
+    void editVeritcal(int x){
+        switch(patternType){
+            case PatternType.Bool:
+                bool newBoolValue = EditorGUILayout.Toggle(boolPatternVertical[x], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newBoolValue != boolPatternVertical[x]){
+                    boolPatternVertical[x] = newBoolValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        boolPattern[x,y] = newBoolValue;
+                    }
+                }
+                break;
+
+            case PatternType.Int:
+                int newIntValue = EditorGUILayout.IntField(intPatternVertical[x],GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newIntValue != intPatternVertical[x]){
+                    intPatternVertical[x] = newIntValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        intPattern[x,y] = newIntValue;
+                    }
+                }
+                break;
+
+            case PatternType.Float:
+                float newFloatValue = EditorGUILayout.FloatField(floatPatternVertical[x], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newFloatValue != floatPatternVertical[x]){
+                    floatPatternVertical[x] = newFloatValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        floatPattern[x,y] = newFloatValue;
+                    }
+                }
+                break;
+
+            case PatternType.String:
+                string newStringValue = EditorGUILayout.TextField(stringPatternVertical[x], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newStringValue != stringPatternVertical[x]){
+                    stringPatternVertical[x] = newStringValue;
+
+                    for(int y = 0; y < lenY; y++){
+                        stringPattern[x,y] = newStringValue;
+                    }
+                }
+                break;
+        }
+    }
+
+    void editHorizontal(int y){
+        switch(patternType){
+            case PatternType.Bool:
+                bool newBoolValue = EditorGUILayout.Toggle(boolPatternHorizontal[y], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newBoolValue != boolPatternHorizontal[y]){
+                    boolPatternHorizontal[y] = newBoolValue;
+
+                    for(int x = 0; x < lenX; x++){
+                        boolPattern[x,y] = newBoolValue;
+                    }
+                }
+                GUILayout.Space(fieldWidth);
+                break;
+
+            case PatternType.Int:
+                int newIntValue = EditorGUILayout.IntField(intPatternHorizontal[y],GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newIntValue != intPatternHorizontal[y]){
+                    intPatternHorizontal[y] = newIntValue;
+
+                    for(int x = 0; x < lenX; x++){
+                        intPattern[x,y] = newIntValue;
+                    }
+                }
+                GUILayout.Space(fieldWidth);
+                break;
+
+            case PatternType.Float:
+                float newFloatValue = EditorGUILayout.FloatField(floatPatternHorizontal[y], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newFloatValue != floatPatternHorizontal[y]){
+                    floatPatternHorizontal[y] = newFloatValue;
+
+                    for(int x = 0; x < lenX; x++){
+                        floatPattern[x,y] = newFloatValue;
+                    }
+                }
+                GUILayout.Space(fieldWidth);
+                break;
+
+            case PatternType.String:
+                string newStringValue = EditorGUILayout.TextField(stringPatternHorizontal[y], GUILayout.Width(fieldWidth), GUILayout.Height(fieldHeight));
+                if(newStringValue != stringPatternHorizontal[y]){
+                    stringPatternHorizontal[y] = newStringValue;
+
+                    for(int x = 0; x < lenX; x++){
+                        stringPattern[x,y] = newStringValue;
+                    }
+                }
+                GUILayout.Space(fieldWidth);
+                break;
+        }
+    }
+
+    Type[,] setPatternArray<Type>(Type[,] pattern){
         if(pattern == null) pattern = new Type[lenX, lenY];
         if(pattern.Length != lenX * lenY) pattern = new Type[lenX, lenY];
         return pattern;
     }
+
+    Type[] setPatternSideArray<Type>(Type[] patternSide, bool isVertical){
+        int len = (isVertical)? lenX: lenY;
+
+        if(patternSide == null) patternSide = new Type[len];
+        if(patternSide.Length != len) patternSide = new Type[len];
+        return patternSide;
+
+    }
+
     private void WriteToJsonFile(string path, string jsonText){
         System.IO.FileStream fileStream = new System.IO.FileStream(path, System.IO.FileMode.Create);
         using(System.IO.StreamWriter streamWriter = new System.IO.StreamWriter(fileStream)){
